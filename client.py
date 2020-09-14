5e815917f27903dc296fa4d831e6f635070df3e2
Aman Kumar Maurya <amanrkt231217@gmail.com>
2020-09-14 06:53:53 +0000

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-HOST = '127.0.0.1'    # The remote host
-PORT = 44444           # The same port as used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect_ex((HOST, PORT))
-    s.sendall(b"hello server!")
-    data = s.recv(1024)
-print('Received', repr(data))
