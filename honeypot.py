ba0d50ba205a801aaac3cb00e50893c7e98bdba8
jayhawk24 <anubhavthrilling24@gmail.com>
2020-09-16 04:43:58 +0530

    minor changes

--- honeypot.py
+++ honeypot.py
@@ -13,13 +13,13 @@ class honey(object):
         self.ports = ports
         self.log_filepath = log_filepath
         self.listener_threads = {}
-        self.logger = self.prepare_logger()
+        self.logger = self.mylogger()
 
         self.logger.info("Honeypot initializing...")
         self.logger.info("Ports: %s" % self.ports)
         self.logger.info("Log filepath: %s" % self.log_filepath)
 
-    def handle_connection(self, client_socket, port, ip, remote_port):
+    def incoming(self, client_socket, port, ip, remote_port):
         self.logger.info(
             "Connection received: %s: %s:%d" %
             (port, ip, remote_port))
@@ -35,7 +35,7 @@ class honey(object):
             pass
         client_socket.close()
 
-    def start_new_listener_thread(self, port):
+    def listening_thread(self, port):
         # Create a new listener
         listener = socket()
         listener.bind((self.bind_ip, int(port)))
@@ -44,20 +44,20 @@ class honey(object):
         while True:
             client, addr = listener.accept()
             client_handler = threading.Thread(
-                target=self.handle_connection, args=(
+                target=self.incoming, args=(
                     client, port, addr[0], addr[1]))
             client_handler.start()
 
     def start_listening(self):
         for port in self.ports:
             self.listener_threads[port] = threading.Thread(
-                target=self.start_new_listener_thread, args=(port,))
+                target=self.listening_thread, args=(port,))
             self.listener_threads[port].start()
 
     def run(self):
         self.start_listening()
 
-    def prepare_logger(self):
+    def mylogger(self):
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(levelname)-8s %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%s',
