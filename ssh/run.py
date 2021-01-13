4e507be67400d1b4612c34fa529944df04b53550
Aman Kumar Maurya <amanrkt231217@gmail.com>
2021-01-13 09:00:04 +0530

    adding new working ssh honeypot...

---
+++ ssh/run.py
@@ -0,0 +1,54 @@
+import ssht
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Python3 ssh honeypot')
+parser.add_argument(
+    'ip',
+    metavar='<IP_Address>',
+    type=str,
+    default='127.0.0.1',
+    help='IP on which Honeypot would be running usually localhost')
+
+parser.add_argument(
+    'port',
+    metavar='<Ports>',
+    type=int,
+    nargs='+',
+    help='IP on which ssh would be running usually port 22')
+
+parser.add_argument(
+    'conn',
+    metavar='<Log_file>',
+    type=str,
+    default='connections.log',
+    help='The File on which Honeypot would be logging any connections')
+
+parser.add_argument(
+    'auth',
+    metavar='<Log_file>',
+    type=str,
+    default='auth.log',
+    help='The File on which Honeypot would be logging any auths')
+
+parser.add_argument(
+    'key_log',
+    metavar='<Log_file>',
+    type=str,
+    default='keys.log',
+    help='The File on which Honeypot would be logging any key auth')
+
+
+args = parser.parse_args()
+print(args)
+
+ip = args.ip
+port = args.port
+logfile1 = args.conn
+logfile2 = args.auth
+logfile3 = args.key_log
+
+print(ip, port, logfile1, logfile2, logfile3)
+
+ssh_run = ssht.sshtp(ip, port, logfile1, logfile2, logfile3)
+ssh_run.run()
