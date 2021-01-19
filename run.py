6488abc4a38eb4524f73ca481a0f1971ea6e7be0
jayhawk24 <anubhavgupta1307@gmail.com>
2021-01-20 02:07:17 +0530

    Merge run files

--- run.py
+++ run.py
@@ -1,36 +1,38 @@
 import honeypot
 import argparse
-import sys
+from ssh import ssht
 
-parser = argparse.ArgumentParser(description='Python3 honeypot')
+parser = argparse.ArgumentParser(description="Python3 honeypot")
 parser.add_argument(
-    'ip',
-    metavar='<IP_Address>',
+    "ip",
+    metavar="<IP_Address>",
     type=str,
-    default='127.0.0.1',
-    help='IP on which Honeypot would be running usually localhost')
+    default="127.0.0.1",
+    help="IP on which Honeypot would be running usually localhost",
+)
+
+parser.add_argument("sshport", metavar="<Ports>", type=int, help="Ports On which ssh honepot would be running.")
+
+parser.add_argument("ports", metavar="<Ports>", type=int, nargs="+", help="Ports On which honepot would be running.")
 
 parser.add_argument(
-    'log',
-    metavar='<Log_file>',
+    "log",
+    metavar="<Log_file>",
     type=str,
-    default='connections.log',
-    help='The File on which Honeypot would be logging any connections')
-
-parser.add_argument(
-    'ports',
-    metavar='<Ports>',
-    type=int,
-    nargs='+',
-    help='IP on which Honeypot would be running usually localhost')
+    default="connections.log",
+    help="The File on which Honeypot would be logging any connections",
+)
 
 args = parser.parse_args()
-print(args)
 
 ip = args.ip
 logfile = args.log
+sshport = [args.sshport]
 port = args.ports
 
 print(ip, logfile, port)
 hon = honeypot.honey(ip, port, logfile)
+ssh_run = ssht.sshtp(ip, sshport, logfile, logfile)
+
 hon.run()
+ssh_run.run()
